e18503fbb6c02cefe4739a7556367e06dda10f73
Elena-Filippova <157601139+Elena-Filippova@users.noreply.github.com>
2026-04-09 15:46:03 +0300

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Courses from './components/courses/Courses'
 import Hero from './components/hero/Hero'
+import Invitation from './components/invitation/Invitation'
+import Reviews from './components/reviews/Reviews'
+import Welcome from './components/welcome/Welcome'
 
 export default function Home() {
   return (
-    <Hero /> 
+    <>    
+      <Hero />
+      <Courses /> 
+      <Invitation />
+      <Reviews />
+      <Welcome />
+    </>
   )
 }
