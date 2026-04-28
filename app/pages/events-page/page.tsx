a0839b06cab2c38b36c02ed54f6ccd8083ffa9d6
Elena-Filippova <157601139+Elena-Filippova@users.noreply.github.com>
2026-04-28 21:36:38 +0300

    Add files via upload

--- app/pages/events-page/page.tsx
+++ app/pages/events-page/page.tsx
@@ -1,6 +1,17 @@
+'use client'
+
 import Events from '@/app/components/events-page/Events'
+import { useHeaderContext } from '@/app/hooks/useHeaderContext'
+import { useEffect } from 'react'
 
 export default function EventsPage() {
+  const { setIsProgramsPage } = useHeaderContext()
+
+  useEffect(() => {
+    setIsProgramsPage(true)
+    return () => setIsProgramsPage(false)
+  }, [setIsProgramsPage])
+
   return (
     <Events />
   )
