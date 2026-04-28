10d2753499a4d50231cee2b315074751fe1a7beb
Elena-Filippova <157601139+Elena-Filippova@users.noreply.github.com>
2026-04-28 12:54:54 +0300

    Add files via upload

--- app/pages/auth-page/page.tsx
+++ app/pages/auth-page/page.tsx
@@ -1,6 +1,17 @@
+'use client'
+
 import AuthForm from '@/app/components/authForm/AuthForm'
+import { useHeaderContext } from '@/app/hooks/useHeaderContext'
+import { useEffect } from 'react'
 
 export default function AuthPage() {
+  const { setIsProgramsPage } = useHeaderContext()
+
+  useEffect(() => {
+    setIsProgramsPage(true)
+    return () => setIsProgramsPage(false)
+  }, [setIsProgramsPage])
+
   return (
     <AuthForm />
   )
--- app/pages/programs-page/page.tsx
+++ app/pages/programs-page/page.tsx
@@ -1,7 +1,16 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useHeaderContext } from '@/app/hooks/useHeaderContext'
 import AllPrograms from '../../components/allPrograms/AllPrograms'
 
 export default function Programs() {
-  return (
-    <AllPrograms />
-  )
+  const { setIsProgramsPage } = useHeaderContext()
+
+  useEffect(() => {
+    setIsProgramsPage(true)
+    return () => setIsProgramsPage(false)
+  }, [setIsProgramsPage])
+
+  return <AllPrograms />
 }
