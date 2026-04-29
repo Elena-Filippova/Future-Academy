6d6eaef773fa9649a8d36fea47319de97d78669f
Elena-Filippova <157601139+Elena-Filippova@users.noreply.github.com>
2026-04-29 18:23:54 +0300

    Add files via upload

--- app/pages/event-page/page.tsx
+++ app/pages/event-page/page.tsx
@@ -1,5 +1,17 @@
+import HeroEvent from '@/app/components/hero-event/HeroEvent'
+import InfoEvent from '@/app/components/info-event/InfoEvent'
+import PhotoEvent from '@/app/components/photo-event/PhotoEvent'
+import ProgramEvent from '@/app/components/program-event/ProgramEvent'
+import TeachersEvent from '@/app/components/teachers-event/TeachersEvent'
+
 export default function EventPage() {
   return (
-    <></>
+    <>
+      <HeroEvent />
+      <InfoEvent />
+      <ProgramEvent/>
+      <TeachersEvent />
+      <PhotoEvent />
+    </>
   )
 }
