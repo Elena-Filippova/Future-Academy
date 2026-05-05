e4ae08cb53d8a4b8638fcbddf501d8b7ff21f686
Elena-Filippova <157601139+Elena-Filippova@users.noreply.github.com>
2026-05-05 14:03:24 +0300

    Add files via upload

---
+++ app/constants/news.js
@@ -0,0 +1,46 @@
+const newsArray = [
+  {
+    id: 0,
+    image: '/news/card-1.webp',
+    date: '16 ноября 2021',
+    title: 'Заголовок новости',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi elit, egestas ut feugiat id, laoreet volutpat risus.',
+  },
+  {
+    id: 1,
+    image: '/news/card-2.webp',
+    date: '16 ноября 2021',
+    title: 'Заголовок новости',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi elit, egestas ut feugiat id, laoreet volutpat risus.',
+  },
+  {
+    id: 2,
+    image: '/news/card-3.webp',
+    date: '16 ноября 2021',
+    title: 'Заголовок новости',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi elit, egestas ut feugiat id, laoreet volutpat risus.',
+  },
+  {
+    id: 3,
+    image: '/news/card-4.webp',
+    date: '16 ноября 2021',
+    title: 'Заголовок новости',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi elit, egestas ut feugiat id, laoreet volutpat risus.',
+  },
+  {
+    id: 4,
+    image: '/news/card-5.webp',
+    date: '16 ноября 2021',
+    title: 'Заголовок новости',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi elit, egestas ut feugiat id, laoreet volutpat risus.',
+  },
+  {
+    id: 5,
+    image: '/news/card-6.webp',
+    date: '16 ноября 2021',
+    title: 'Заголовок новости',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mi elit, egestas ut feugiat id, laoreet volutpat risus.',
+  },
+]
+
+export default newsArray
